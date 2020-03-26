c48903ac976e6f93629bd44cff57d44848059ff1
Noah Tanner <noah@Noahs-MacBook-Pro.local>
2020-03-26 15:36:48 -0400

    set up for pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='iniPy',
+      version='0.1',
+      description='a lightweight python project initalizer',
+      author='kevinshome',
+      author_email='',
+      url='https://github.com/kevinshome',
+      packages=['inipy'],
+      package_dir={'':'.'}
+     )
